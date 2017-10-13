efebff619a3b5ca7426bb99702fb462ed262131f
arnaubt <arnaubt@gmail.com>
2017-10-13 23:00:41 +0200

    Added project structure

---
+++ src/app-client.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { App } from './components/App';
+
+const AppClient = () => (
+    <Router>
+        <App />
+    </Router>
+);
+
+window.onload = () => {
+    render(<AppClient />, document.getElementById('main'));
+};
---
+++ src/server.js
@@ -0,0 +1,7 @@
+import path from 'path';
+import { Server } from 'http';
+import Express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter as Router } from 'react-router-dom';
+import { App } from './components/App';
